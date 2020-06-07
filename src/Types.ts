b83ee64e99711ff6d0455aee809a1c52067f5fd3
Phillip Sgardelis <Phillip@Cognativ.com.au>
2020-06-07 20:01:32 +1000

    Feat(Types): Define data types

---
+++ src/Types.ts
@@ -0,0 +1,43 @@
+import { firestore } from 'firebase-admin';
+
+// App Wide Data Types
+
+
+// Common Types
+
+export type Uri = string;
+export type DateTime = firestore.Timestamp;
+
+// Entities
+
+export interface User {
+  id: string;
+  name: string;
+  activeStory: StoryView['id'];
+};
+
+export interface Story {
+  id: string;
+  authorId: User['id'];
+  published: boolean;
+  metadata: {
+    coverPhoto: Uri;
+    description: string;
+    failureMessage: string;
+    genre: string;
+    title: string;
+  };
+}
+
+export interface StoryView {
+  id: string;
+  storyId: Story['id'];
+  stepCounter: number;
+  messages: {
+    fbMessageId: string;
+    stepCounter: number;
+    text: string;
+  }[];
+  startTime: DateTime;
+  endTime: DateTime | null;
+};
