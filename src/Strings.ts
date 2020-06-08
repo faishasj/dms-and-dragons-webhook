f040b7f48b8c098b638a66e501a28a135c43dd57
Phillip Sgardelis <Phillip@Cognativ.com.au>
2020-06-08 20:12:39 +1000

    Feat(Strings): Create Strings abstraction with greeting and intro

---
+++ src/Strings.ts
@@ -0,0 +1,4 @@
+export default {
+  greeting: (name: string) => `Greetings ${name}, to *DMs and Dragons*, and interactive messaging experience based on storytelling in the age of 'sliding into the DMs.'`,
+  intro: 'Will it be a nail-biting creepypasta horror story, or a modern romance a la Romeo and Juliet... if they had Facebook accounts? DMs and Dragons allows you to get your creative juices flowing and create your own messaging story to share with your friends, or the world! Here are some stories other users have created:',
+};
