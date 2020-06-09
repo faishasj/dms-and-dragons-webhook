1a43d81767f8747ee4b272c2e2121fa586ae5278
faishasj <faishasj@gmail.com>
2020-06-09 12:00:23 +1000

    feat(Intro): Refactor greeting messages

--- src/Strings.ts
+++ src/Strings.ts
@@ -1,6 +1,6 @@
 export default {
-  greeting: (name: string) => `Greetings ${name}, to *DMs and Dragons*, an interactive messaging experience based on storytelling in the age of 'sliding into the DMs.'`,
-  intro1: 'Will it be a nail-biting creepypasta horror story, or a modern romance a la Romeo and Juliet... if they had Facebook accounts?',
-  intro2: 'DMs and Dragons allows you to get your creative juices flowing and create your own messaging story to share with your friends, or the world!',
+  greeting: (name: string) => `Greetings, ${name}, to DMs and Dragons 🐉!`,
+  intro1: 'Will it be a nail-biting creepypasta horror story 👻, or a modern romance a la Romeo and Juliet... if they had Facebook accounts 😍?',
+  intro2: 'DMs and Dragons 🐉 allows you to get your creative juices flowing and create your own messaging story to share with your friends, or the world!',
   intro3: 'Here are some stories other users have created:',
 };
